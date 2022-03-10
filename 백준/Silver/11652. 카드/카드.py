aabd37d006f9593b8d9a3cc231468bf60999f184
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-03-11 01:13:05 +0900

    [Silver IV] Title: 카드, Time: 184 ms, Memory: 53928 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+dic = {}
+
+for _ in range(n):
+    card = int(sys.stdin.readline())
+    if card in dic:
+        dic[card] += 1
+    else:
+        dic[card] = 1
+
+result = sorted(dic.items(), key=lambda x:(-x[1], x[0]))
+print(result[0][0])
