2fcd8f2719fea40c047cb409015f222008d132be
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-03-17 16:55:10 +0900

    [Gold V] Title: LCS, Time: 644 ms, Memory: 55052 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+s1 = input().strip()
+s2 = input().strip()
+d = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1] == s2[j-1]:
+            d[i][j] = d[i-1][j-1] + 1
+        else:
+            d[i][j] = max(d[i-1][j], d[i][j-1])
+print(d[-1][-1])
