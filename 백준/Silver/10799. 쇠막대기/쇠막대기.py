30289c978ed826d8879cc348059ca6533fc229fe
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-04-25 23:25:46 +0900

    [Silver III] Title: 쇠막대기, Time: 108 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,15 @@
+s = list(input())
+answer = 0
+stack = []
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+    else:
+        if s[i-1] == '(':
+            stack.pop()
+            answer += len(stack)
+        else:
+            stack.pop()
+            answer += 1
+print(answer)
