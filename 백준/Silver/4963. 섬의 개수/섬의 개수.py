ab7caa43f3ddc1f2076289dd941311825161bdeb
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-05-04 00:06:01 +0900

    [Silver II] Title: 섬의 개수, Time: 120 ms, Memory: 33132 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,19 +1,19 @@
-from collections import deque
+from sys import*
+setrecursionlimit(10**6)
 
-def bfs(x, y):
+def dfs(x, y):
     dx = [-1, -1, -1, 0, 0, 1, 1, 1]
     dy = [-1, 0, 1, -1, 1, -1, 0, 1]
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
+    if x < 0 or x >= h or y < 0 or y >= w:
+        return False
+    if maps[x][y] == 1:
         maps[x][y] = 0
         for i in range(8):
             nx = x+dx[i]
             ny = y+dy[i]
-            if nx >= 0 and nx < h and ny >= 0 and ny < w and maps[nx][ny] == 1:
-                maps[nx][ny] = 0
-                queue.append((nx, ny))
+            dfs(nx, ny)
+        return True
+    return False
     
 while True:
     w, h = map(int, input().split())
@@ -27,6 +27,6 @@ while True:
     for i in range(h):
         for j in range(w):
             if maps[i][j] == 1:
-                bfs(i, j)
+                dfs(i, j)
                 count += 1
     print(count)
