159eb10c683f8eb3bb4dc87aab7ac786b48f082a
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-05-03 18:21:12 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,24 @@
 n = int(input())
 m = int(input())
 graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-count = 0
+visited = [False]*(n+1)
 
-for i in range(m):
-    a, b = map(int , input().split())
+for _ in range(m):
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
+
 for i in range(n+1):
     graph[i].sort()
 
-def bfs(v):
+count = 0
+def dfs(v):
     visited[v] = True
     global count
+    count += 1
     for i in graph[v]:
         if not visited[i]:
-            bfs(i)
-            count += 1
+            dfs(i)
 
-bfs(1)
-print(count)
+dfs(1)
+print(count-1)
