1cf0a9f9799c10b9c05d05363c4049fcec951771
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-05-06 14:16:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 180 ms, Memory: 34040 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,16 +1,18 @@
-n = int(input())
-d = []
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+dp = []
 for _ in range(n):
-    d.append(list(map(int, input().split())))
+    dp.append(list(map(int, input().split())))
 
 for i in range(1, n):
     for j in range(i+1):
         if j == 0:
-            d[i][j] = d[i][j] + d[i-1][j]
+            dp[i][j] = dp[i][j] + dp[i-1][j]
         elif j == i:
-            d[i][j] = d[i][j] + d[i-1][j-1]
+            dp[i][j] = dp[i][j] + dp[i-1][j-1]
         else:
-            d[i][j] = d[i][j] + max(d[i-1][j-1], d[i-1][j])
+            dp[i][j] = dp[i][j] + max(dp[i-1][j-1], dp[i-1][j])
             
-print(max(d[n-1]))
+print(max(dp[n-1]))
