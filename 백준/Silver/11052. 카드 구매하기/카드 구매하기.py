69ddf4a0a6f6f139649f76c4fbef47f81953cac0
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-05-20 23:42:37 +0900

    [Silver I] Title: 카드 구매하기, Time: 180 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+dp = [0]*(n+1)
+
+dp[1] = arr[1]
+for i in range(2, n+1):
+    for j in range(1, i+1):
+        if dp[i] < dp[i-j] + arr[j]:
+            dp[i] = dp[i-j] + arr[j]
+
+print(dp[n])
