2a48a6a7b8fcb0033be8ca2fb6f0648d78a94aeb
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-07-16 13:06:45 +0900

    [D2] Title: 스도쿠 검증, Time: 151 ms, Memory: 58,520 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -4,24 +4,24 @@ for tc in range(1, t+1):
     for _ in range(9):
         arr.append(list(map(int, input().split())))
 
+    x_test = [0]*10
     flag = True
-    for j in range(9):
-        x_test = [0]*10
-        for i in range(9):
-            x_test[arr[j][i]] += 1
+    for i in range(9):
+        for j in range(9):
+            x_test[arr[i][j]] += 1
         for z in range(1, 10):
-            if x_test[z] != 1:
+            if x_test[z] != i+1:
                 flag = False
                 break
         if flag == False:
             break
 
-    for j in range(9):
-        y_test = [0]*10
-        for i in range(9):
-            y_test[arr[i][j]] += 1
+    y_test = [0]*10
+    for i in range(9):
+        for j in range(9):
+            y_test[arr[j][i]] += 1
         for z in range(1, 10):
-            if y_test[z] != 1:
+            if y_test[z] != i+1:
                 flag = False
                 break
         if flag == False:
@@ -29,12 +29,13 @@ for tc in range(1, t+1):
 
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
-            s_test = [0]*10
-            for z in range(i, i+3):
-                for k in range(j, j+3):
-                    s_test[arr[z][k]] += 1
-            for m in range(1, 10):
-                if s_test[m] != 1:
+            test = [0]*10
+            for x in range(i, i+3):
+                for y in range(j, j+3):
+                    test[arr[x][y]] += 1
+
+            for s in range(1, 10):
+                if test[s] != 1:
                     flag = False
                     break
             if flag == False:
@@ -42,7 +43,7 @@ for tc in range(1, t+1):
         if flag == False:
             break
 
-    if flag == False:
-        print(f"#{tc} 0")
+    if flag == True:
+        print("#{} {}" .format(tc, 1))
     else:
-        print(f"#{tc} 1")
+        print("#{} {}" .format(tc, 0))
