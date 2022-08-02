1743d073c97cd00c07f3787810a04e8a40174254
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-08-02 22:16:04 +0900

    [D2] Title: 달팽이 숫자, Time: 173 ms, Memory: 58,276 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,29 +1,57 @@
-t = int(input())
-for tc in range(1, t+1):
-    n = int(input())
-    arr = [[0]*n for _ in range(n)]
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    x, y, num = 0, 0, 1
-    arr[x][y] = num
-    target = n**2
-    while True:
-        for i in range(4):
-            while True:
-                x += dx[i]
-                y += dy[i]
-                if x<0 or x>=n or y<0 or y>=n or arr[x][y] != 0:
-                    x -= dx[i]
-                    y -= dy[i]
-                    break
-                else:
-                    num += 1
-                    arr[x][y] = num
-        if num == target:
-            break
-        
-    print("#{}" .format(tc))
-    for i in range(n):
-        for j in range(n):
-            print(arr[i][j], end=' ')
-        print()
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<t+1; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			int[][] arr = new int[n][n];
+			int num = 2;
+			int[] dx = {0, 1, 0, -1};
+			int[] dy = {1, 0, -1, 0};
+			int x = 0;
+			int y = 0;
+			arr[x][y] = 1;
+			
+			int nx = 0;
+			int ny = 0;
+			while(true) {
+				for(int i=0; i<4; i++) {
+					while(true) {
+						x += dx[i];
+						y += dy[i];
+						if(x>=0 && x<n && y>=0 && y<n && arr[x][y]==0) {
+							arr[x][y] = num;
+							num += 1;
+						} else {
+							x -= dx[i];
+							y -= dy[i];
+							break;
+						}
+					}
+				}
+				if(num==n*n+1) break;
+			}//while
+			
+			System.out.println("#"+tc);
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<n; j++) {
+					if(j==n-1) System.out.print(arr[i][j]);
+					else System.out.print(arr[i][j]+" ");
+				}
+				System.out.println();
+			}
+			
+			
+		}//for (test_case)
+
+	}
+
+}
