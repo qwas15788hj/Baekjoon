9b3d554c64e000247e55634c332e676319184855
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-09-29 21:26:13 +0900

    [level 3] Title: 징검다리 건너기, Time: 249.93 ms, Memory: 18.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,24 @@
+def solution(stones, k):
+    answer = 0
+    left = 1
+    right = 200000000
+    
+    while left <= right:
+        mid = (left+right)//2
+        count = 0
+        for stone in stones: #돌마다
+            if stone-mid <= 0: #돌-건넌수 = 음수면
+                count += 1 #못건너는 돌
+            else:
+                count = 0 #건너감
+            
+            if count >= k: #이미 못건넘
+                break
+        
+        if count >= k: #못건너면 건너는 수 줄이기
+            right = mid-1
+        else: #건널수 있으니 저장
+            left = mid+1
+            answer = left
+            
+    return left
