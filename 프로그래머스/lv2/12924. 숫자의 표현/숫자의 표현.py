dab43529224ba6b3e738d916239ae7257f7c8527
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2023-06-16 02:18:47 +0900

    [level 2] Title: 숫자의 표현, Time: 1.70 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(n):
+    answer = 0
+    count = 0
+    arr = deque()
+    
+    for i in range(1, n+1):
+        count += i
+        arr.append(i)
+        if count == n:
+            answer += 1
+        elif count > n:
+            while True:
+                count -= arr.popleft()
+                if count == n:
+                    answer += 1
+                elif count < n:
+                    break
+    
+    return answer
