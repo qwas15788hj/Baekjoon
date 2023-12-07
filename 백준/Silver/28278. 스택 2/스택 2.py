691651f6d26e287dbfeb35edd6fcef242cd5d4af
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2023-12-07 17:07:12 +0900

    [Silver IV] Title: 스택 2, Time: 484 ms, Memory: 184100 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+for _ in range(n):
+    com = list(map(int, input().split()))
+    if com[0] == 1:
+        stack.append(com[1])
+    elif com[0] == 2:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif com[0] == 3:
+        print(len(stack))
+    elif com[0] == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
