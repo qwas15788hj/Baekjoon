aff3393739a3470124c3fbd2de3555cec51e105f
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2023-12-11 16:34:39 +0900

    [Silver IV] Title: 가로수, Time: 4088 ms, Memory: 40952 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,26 +1,18 @@
-def gcd(x, y):
-    if x%y == 0:
-        return y
-    else:
-        return gcd(y, x%y)
+import math
 
 n = int(input())
-tree = []
-arr = []
-for _ in range(n):
-    tree.append(int(input()))
-
+arr = [int(input()) for _ in range(n)]
+arr.sort()
+sub = []
 for i in range(n-1):
-    arr.append(tree[i+1]-tree[i])
+    sub.append(arr[i+1] - arr[i])
 
-dp = [0]*(n-2)
-dp[0] = gcd(arr[0], arr[1])
-for i in range(1, n-2):
-    dp[i] = gcd(dp[i-1], arr[i+1])
+gcd = sub[0]
+for i in range(1, len(sub)):
+    gcd = math.gcd(gcd, sub[i])
 
-gcd_num = dp[-1]
-count = 0
-for i in arr:
-    count += (i//gcd_num)-1
+answer = 0
+for i in range(len(sub)):
+    answer += sub[i]//gcd - 1
 
-print(count)
+print(answer)
