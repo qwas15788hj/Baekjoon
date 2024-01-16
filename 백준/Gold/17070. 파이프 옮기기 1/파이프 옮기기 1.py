86bf54d9e77304236822f678c8288acec524c1ca
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-01-16 17:45:06 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,30 +1,16 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-pipe = [[[] for _ in range(n)] for _ in range(n)]
-pipe[0][1].append(1)
+pipe = [[[0] * 3 for _ in range(n)] for _ in range(n)]
 
+pipe[0][1][0] = 1
 for i in range(n):
-    for j in range(n):
-        for z in range(len(pipe[i][j])):
-            if pipe[i][j][z] == 1:
-                if j+1 < n and arr[i][j+1] == 0:
-                    pipe[i][j+1].append(1)
-                if j+1 < n and i+1 < n and arr[i][j+1] == 0 and arr[i+1][j] == 0 and arr[i+1][j+1] == 0:
-                    pipe[i+1][j+1].append(2)
-            elif pipe[i][j][z] == 2:
-                if j+1 < n and arr[i][j+1] == 0:
-                    pipe[i][j+1].append(1)
-                if j+1 < n and i+1 < n and arr[i][j+1] == 0 and arr[i+1][j] == 0 and arr[i+1][j+1] == 0:
-                    pipe[i+1][j+1].append(2)
-                if i+1 < n and arr[i+1][j] == 0:
-                    pipe[i+1][j].append(3)
-            else:
-                if j+1 < n and i+1 < n and arr[i][j+1] == 0 and arr[i+1][j] == 0 and arr[i+1][j+1] == 0:
-                    pipe[i+1][j+1].append(2)
-                if i+1 < n and arr[i+1][j] == 0:
-                    pipe[i+1][j].append(3)
+    for j in range(1, n):
+        if i == 0 and j == 1:
+            continue
+        if arr[i][j] == 0:
+            pipe[i][j][0] = pipe[i][j-1][0] + pipe[i][j-1][2]
+            pipe[i][j][1] = pipe[i-1][j][1] + pipe[i-1][j][2]
+            if arr[i-1][j] == 0 and arr[i][j-1] == 0:
+                pipe[i][j][2] = pipe[i-1][j-1][0] + pipe[i-1][j-1][1] + pipe[i-1][j-1][2]
 
-print(len(pipe[n-1][n-1]))
+print(sum(pipe[n-1][n-1]))
