ef56dbec0434ae7b4ee3599428d50be00abbdd8e
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-01-22 22:24:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 4904 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+w, v = [0], [0]
+for _ in range(n):
+    a, b = map(int, input().split())
+    w.append(a)
+    v.append(b)
+
+arr = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1, n+1): # 물품
+    for j in range(1, k+1): # 가방 무게
+        if w[i] > j:
+            arr[i][j] = arr[i-1][j]
+        else:
+            arr[i][j] = max(arr[i-1][j], arr[i-1][j-w[i]] + v[i])
+
+print(arr[-1][-1])
