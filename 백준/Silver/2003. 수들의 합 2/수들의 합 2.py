865a81f01519f5f30824c7afb32a4247bf65742d
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-01-25 19:26:06 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,16 +1,14 @@
-n, m = map(int, input().split()) # n, m 입력
-arr = list(map(int, input().split())) # 숫자 입력
-store = [] # 숫자 저장할 배열
-num, answer = 0, 0 # 현재 수, 경우의 수
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+num, left = 0, 0
+answer = 0
 
-for i in range(n): # 배열 돌면서
-    store.append(arr[i]) # 현재 원소 store에 저장
-    num += arr[i] # 현재 원소 num에 더하기
+for i in range(n):
+    num += arr[i]
+    while num > m:
+        num -= arr[left]
+        left += 1
 
-    while num > m: # 현재 수가 m보다 크면
-        num -= store.pop(0) # 가장 앞 수 빼기
-
-    # num이 m과 같다면 answer 증가
     if num == m:
         answer += 1
 
