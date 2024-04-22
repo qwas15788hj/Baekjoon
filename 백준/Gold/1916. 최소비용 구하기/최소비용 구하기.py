74405f1202c4449020ab691e35172e636b0a9c7a
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-04-22 22:37:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 468 ms, Memory: 134852 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,26 @@
+import heapq, sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+arr = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    arr[a].append([b, c])
+visited = [1e9] * (n+1)
+start, end = map(int, input().split())
+
+queue = []
+heapq.heappush(queue, [start, 0])
+visited[start] = 0
+while queue:
+    now, dist = heapq.heappop(queue)
+    if visited[now] < dist:
+        continue
+    for i in arr[now]:
+        cost = dist + i[1]
+        if cost < visited[i[0]]:
+            visited[i[0]] = cost
+            heapq.heappush(queue, [i[0], cost])
+
+print(visited[end])
