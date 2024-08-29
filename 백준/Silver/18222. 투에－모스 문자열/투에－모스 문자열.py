155f210d9650dd7d9baaa3b52f452bf993b6ecd3
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-08-29 17:03:00 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.py
@@ -0,0 +1,13 @@
+k = int(input())
+
+def find(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    elif x % 2 == 0:
+        return find(x//2)
+    else:
+        return 1-find(x//2)
+
+print(find(k-1))
