3ab10050e3d9b937fe4cfa89925fc31ae22600eb
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2024-11-28 22:52:38 +0900

    [Silver V] Title: Q-인덱스, Time: 112 ms, Memory: 110856 KB -BaekjoonHub

--- 백준/Silver/13333. Q－인덱스/Q－인덱스.py
+++ 백준/Silver/13333. Q－인덱스/Q－인덱스.py
@@ -7,12 +7,7 @@ arr = list(map(int, input().split()))
 num = [0] * (max(arr)+1)
 for i in range(n):
     num[arr[i]] += 1
-
-pre_num = [0] * (max(arr)+1)
-pre_num[0] = num[0]
-for i in range(1, len(num)):
-    pre_num[i] = pre_num[i-1] + num[i]
-
+    
 for i in range(len(num)):
     if sum(num[i:]) >= i and sum(num[:i+1]) >= n-i:
         print(i)
