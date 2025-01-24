05b0402d42b99469e29bf21f3bb2cd65f0a7bdd7
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-01-24 14:47:42 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,34 @@
 from collections import deque
 
+# 입력받기
 n, m = map(int, input().split())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input())))
+grid = [list(map(int, input().strip())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 이동 방향 (상, 우, 하, 좌)
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return graph[n-1][m-1]
+    global result  # 전역 변수 result 사용
+    q = deque()
+    q.append((x, y))
+    grid[x][y] = 0  # 시작점 방문 처리
+    result = 1  # 시작점 포함 경로 길이 초기화
+    while q:
+        size = len(q)
+        for _ in range(size):
+            x, y = q.popleft()
+            if x == n-1 and y == m-1:
+                return result
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 1:
+                    q.append((nx, ny))
+                    grid[nx][ny] = 0
+
+        result += 1
+
 
 print(bfs(0, 0))
