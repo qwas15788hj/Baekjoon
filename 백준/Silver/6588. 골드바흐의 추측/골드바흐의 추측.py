ca84855712e2ccf24521b5132e64ba7c40dda3c0
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-02-06 10:32:20 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 284 ms, Memory: 119232 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,22 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-sieve = [False, False] + ([True]*(1000000-1))
-for i in range(2, int(1000001**0.5)+1):
-    if sieve[i] == True:
-        for j in range(i*2, 1000000+1, i):
-            sieve[j] = False
+arr = [True] * 1000001
+arr[0] = False
+arr[1] = False
+for i in range(2, len(arr)):
+    if arr[i] == True:
+        j = 2
+
+        while (i * j) < len(arr):
+            arr[i * j] = False
+            j += 1
 
 while True:
     n = int(input())
     if n == 0:
         break
 
-    flag = False
-    for i in range(3, len(sieve)):
-        if sieve[i] and sieve[n-i]:
-            print(f"{n} = {i} + {n-i}")
-            flag = True
+    for i in range(1, len(arr), 2):
+        if arr[i] and arr[n-i]:
+            print(f'{n} = {i} + {n-i}')
             break
-    if flag == False:
+    else:
         print("Goldbach's conjecture is wrong.")
