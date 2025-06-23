45716a742705f21dfab5a679526f5dd6210a926d
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-06-23 21:31:26 +0900

    [Gold IV] Title: 좋은수열, Time: 84 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,20 +1,25 @@
 arr = ["1", "2", "3"]
+
+# 좋은 수열인지 확인
 def check(num):
     for i in range(-1, -len(num)//2-1, -1):
+        # 뒤쪽 i길이 부분과 바로 앞쪽 i길이 부분이 같으면 안 됨
         if num[i*2:i] == num[i:]:
             return False
     return True
 
+# 백트래킹을 통해 수열 구성
 def func(num):
     if len(num) == n:
         print(num)
-        exit()
+        exit() # 사전순 가장 앞선 수열 출력 후 종료
 
     for i in arr:
         next_num = num + i
         if check(next_num):
             func(next_num)
 
+# 입력 및 초기 시작
 n = int(input())
 for i in arr:
     func(i)
