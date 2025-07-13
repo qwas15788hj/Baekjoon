4ccc8a32c44fb8e5e79390d4a4b686251ee0cfe0
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-07-13 22:20:37 +0900

    [Gold V] Title: 흙길 보수하기, Time: 136 ms, Memory: 111884 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -1,20 +1,25 @@
+import sys
+input = sys.stdin.readline
+
 n, l = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-arr.sort()
+arr.sort() # 시작 위치 기준으로 정렬
 answer = 0
-plank = -1
+plank = -1 # 널빤지를 마지막으로 덮은 위치
 for s, e in arr:
+    # 이미 널빤지로 덮인 웅덩이면 패스
     if plank >= e:
         continue
-    s = max(s, plank) # 널판지 시작점
+    # 아직 덮이지 않은 경우, 시작 지점 보정
+    s = max(s, plank)
 
-    # 이번에 놓을 널판지 개수
+    # 필요한 널빤지 개수 계산
     count = (e-s)//l
     if (e-s)%l != 0:
         count += 1
 
     answer += count
-    plank = (s + (l*count))
+    plank = (s + (l*count)) # 널빤지 끝 위치 갱신
 
 print(answer)
