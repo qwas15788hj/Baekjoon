b3c9dc68e08adaa29968a92e3ad15cde54044da6
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-10-19 15:06:51 +0900

    [Gold IV] Title: 좋다, Time: 984 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -4,16 +4,17 @@ arr = list(map(int, input().split()))
 arr.sort()
 answer = 0
 for i in range(n):
+    target = arr[i]
     sub_arr = arr[:i] + arr[i+1:]
     left, right = 0, len(sub_arr)-1
 
     while left < right:
         total = sub_arr[left] + sub_arr[right]
-        if total < arr[i]:
-            left += 1
-        elif total > arr[i]:
+        if total > target:
             right -= 1
-        else:
+        elif total < target:
+            left += 1
+        elif total == target:
             answer += 1
             break
 
