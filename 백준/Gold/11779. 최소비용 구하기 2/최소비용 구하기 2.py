71fed5d1f1384571e3a194dc9b5660de07f77181
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-11-15 00:08:03 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 384 ms, Memory: 71544 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -5,36 +5,28 @@ from heapq import heappush, heappop
 n = int(input())
 m = int(input())
 arr = [[] for _ in range(n+1)]
-for _ in range(m):
+for i in range(m):
     a, b, c = map(int, input().split())
     arr[a].append([b, c])
 s, e = map(int, input().split())
 
 queue = []
-heappush(queue, [0, s])
+heappush(queue, [0, s, [s]])
 visited = [1e9] * (n+1)
 visited[s] = 0
-target = [[] for _ in range(n+1)]
+
+check = []
 while queue:
-    c, x = heappop(queue)
+    d, x, v = heappop(queue)
+    if visited[x] < d:
+        continue
     if x == e:
-        break
+        check = v
+    for nx, nd in arr[x]:
+        if visited[nx] > d + nd:
+            visited[nx] = d + nd
+            heappush(queue, [d + nd, nx, v + [nx]])
 
-    for nx, nc in arr[x]:
-        if visited[nx] > c + nc:
-            heappush(queue, [c+nc, nx])
-            visited[nx] = c + nc
-            target[nx] = [x]
-
-answer = [e]
-x = e
-while True:
-    nx = target[x][0]
-    answer.append(nx)
-    x = nx
-    if nx == s:
-        break
-    
 print(visited[e])
-print(len(answer))
-print(*answer[::-1])
+print(len(check))
+print(*check)
