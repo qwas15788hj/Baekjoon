3f40f91c8333e7c0f22a38840e99fa4ff5dcde0f
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2025-11-20 00:09:01 +0900

    [Gold III] Title: 파티, Time: 1144 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,35 +1,33 @@
-import heapq, sys
-input = sys.stdin.readline
+from heapq import heappush, heappop
+
+def Dijkstra(start):
+    queue = []
+    heappush(queue, [0, start])
+    visited = [1e9] * (n+1)
+    visited[start] = 0
+    while queue:
+        c, p = heappop(queue)
+        if visited[p] < c:
+            continue
+
+        for nx, nc in arr[p]:
+            if visited[nx] > c + nc:
+                visited[nx] = c + nc
+                heappush(queue, [c + nc, nx])
+
+    return visited
 
 n, m, x = map(int, input().split())
 arr = [[] for _ in range(n+1)]
-
 for _ in range(m):
-    s, e, d = map(int, input().split())
-    arr[s].append((e, d))
+    a, b, t = map(int, input().split())
+    arr[a].append([b, t])
 
-answer = [0] * (n+1)
+answer = 0
+check = Dijkstra(x)
+for i in range(1, n+1):
+    if i == x:
+        continue
+    answer = max(answer, check[i] + Dijkstra(i)[x])
 
-for j in range(1, n+1):
-    queue = []
-    visited = [1e9] * (n+1)
-    heapq.heappush(queue, (j, 0))
-    visited[j] = 0
-
-    while queue:
-        now, dist = heapq.heappop(queue)
-        if visited[now] < dist:
-            continue
-        for i in arr[now]:
-            cost = dist + i[1]
-            if cost < visited[i[0]]:
-                visited[i[0]] = cost
-                heapq.heappush(queue, (i[0], cost))
-
-    if j == x:
-        for i in range(1, n+1):
-            answer[i] += visited[i]
-    else:
-        answer[j] += visited[x]
-
-print(max(answer))
+print(answer)
