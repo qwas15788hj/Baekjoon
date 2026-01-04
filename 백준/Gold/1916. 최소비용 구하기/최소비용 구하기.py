ab45020e7bbc0c534a75925558bc79e7230b8763
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-04 22:28:43 +0900

    [Gold V] Title: 최소비용 구하기, Time: 2752 ms, Memory: 61304 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,26 +1,24 @@
-import heapq, sys
-input = sys.stdin.readline
+from heapq import heappush, heappop
 
 n = int(input())
 m = int(input())
 arr = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
-    arr[a].append((b, c))
-visited = [1e9] * (n+1)
-start, end = map(int, input().split())
+    arr[a].append([b, c])
+s, e = map(int, input().split())
 
 queue = []
-heapq.heappush(queue, (start, 0))
-visited[start] = 0
+heappush(queue, [0, s])
+visited = [1e9] * (n+1)
+visited[s] = 0
 while queue:
-    now, dist = heapq.heappop(queue)
-    if visited[now] < dist:
+    c, x = heappop(queue)
+    if visited[x] < c:
         continue
-    for i in arr[now]:
-        cost = dist + i[1]
-        if visited[i[0]] > cost:
-            visited[i[0]] = cost
-            heapq.heappush(queue, (i[0], cost))
+    for nx, nc in arr[x]:
+        if visited[nx] > c + nc:
+            visited[nx] = c + nc
+            heappush(queue, [c + nc, nx])
 
-print(visited[end])
+print(visited[e])
