0c8202a6b1ca0ee55de4b20a43860fd01f842e67
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-05 23:22:49 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,21 +1,18 @@
 n, s = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-# 누적 합 사용
-for i in range(1, len(arr)):
+
+for i in range(1, n+1):
     arr[i] += arr[i-1]
 
 answer = 1e9
-left, right = 0, 0
-
-while left != len(arr): # 왼쪽 포인터가 끝까지 도달할 때까지
-    if arr[right] - arr[left] >= s: # 오른쪽 포인터 누적합 - 왼쪽 포인터 누적합이 s보다 크거나 같으면
-        answer = min(answer, right - left) # 둘 차이와 answer 중 작은 것으로 갱신
-        left += 1 # 왼쪽 포인터 증가
-    else: # s보다 작으면
-        if right == len(arr) - 1: # 오른쪽 포인터가 끝까지 도달했다면
-            left += 1 # 왼쪽 포인터 증가
-        else: # 오른쪽 포인터가 끝까지 도달안했다면
-            right += 1 # 오른쪽 포인터 증가
+left, right = 0, 1
+while right < len(arr):
+    num = arr[right] - arr[left]
+    if num >= s:
+        answer = min(answer, right - left)
+        left += 1
+    else:
+        right += 1
 
 if answer == 1e9:
     print(0)
