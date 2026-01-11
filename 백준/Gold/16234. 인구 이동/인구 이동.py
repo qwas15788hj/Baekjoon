1e7fc68356a84e7625e3f1500981d9dea64314ab
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-11 21:30:35 +0900

    [Gold IV] Title: 인구 이동, Time: 6304 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -2,50 +2,46 @@ from collections import deque
 
 n, l, r = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-time = 0
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y, visited, num, total, count):
-    queue = deque([])
-    queue.append([x, y])
-    visited[x][y] = num
-    total[num] = arr[x][y]
-    count[num] = 1
-
+def bfs(x, y, check):
+    queue = deque([[i, j]])
+    visited[i][j] = check
+    city[check] = 1
+    person[check] = arr[x][y]
     while queue:
-        nx, ny = queue.popleft()
-        for i in range(4):
-            mx = nx + dx[i]
-            my = ny + dy[i]
-            if 0 <= mx < n and 0 <= my < n and l <= abs(arr[nx][ny]-arr[mx][my]) <= r and visited[mx][my] == 0:
-                queue.append([mx, my])
-                visited[mx][my] = num
-                total[num] += arr[mx][my]
-                count[num] += 1
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < n and l <= abs(arr[nx][ny] - arr[x][y]) <= r and visited[nx][ny] == 0:
+                queue.append([nx, ny])
+                visited[nx][ny] = check
+                city[check] += 1
+                person[check] += arr[nx][ny]
 
-
-while True:
-    visited = [[0]*n for _ in range(n)]
-    num = 1
-    total = dict()
-    count = dict()
+flag = True
+answer = 0
+while flag:
+    visited = [[0] * n for _ in range(n)]
+    check = 1
+    city = dict()
+    person = dict()
     for i in range(n):
         for j in range(n):
             if visited[i][j] == 0:
-                bfs(i, j, visited, num, total, count)
-                num += 1
-
-    sub_arr = [[0] * n for _ in range(n)]
+                bfs(i, j, check)
+            check += 1
+            
+    if len(city) == n**2:
+        flag = False
     for i in range(n):
         for j in range(n):
-            sub_arr[i][j] = total[visited[i][j]]//count[visited[i][j]]
+            idx = visited[i][j]
+            arr[i][j] = person[idx]//city[idx]
+            
+    answer += 1
 
-    if arr == sub_arr:
-        break
-    else:
-        arr = sub_arr
-        time += 1
-
-print(time)
+print(answer-1)
