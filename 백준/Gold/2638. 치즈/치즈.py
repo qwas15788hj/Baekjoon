8fcb811f8f249c959ef8ebfffb3b717508aab06e
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-18 16:29:30 +0900

    [Gold III] Title: 치즈, Time: 260 ms, Memory: 117800 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,44 +1,52 @@
+import sys
 from collections import deque
-n, m = map(int, input().split()) # n, m 입력
-cheeze = [] # 치즈 배열 생성
-for _ in range(n): # 배열 입력
-    cheeze.append(list(map(int, input().split())))
+input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0] # 상하좌우
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-answer = 0 # 총 걸린 시간
-while True: # 무한 루프 시작
-    total = 0 # 치즈 배열의 총합
-    for i in range(n): # 총합 구하기
-        total += sum(cheeze[i])
-    if total == 0: # 총합이 0이면 다 녹았음으로 무한 루프 종료
-        break
 
-    queue = deque() # 큐 생성
-    check = [[False] * m for _ in range(n)] # 외부 공기인지 내부 공기인지 체크
-    queue.append([0, 0]) # 큐에 맨 처음 시작점 (무조건 외부 공기) 저장
-    check[0][0] = True # 시작점 체크
-    # 외부 공기만 큐에 저장해서 치즈가 몇 개의 외부공기와 만나는지 확인
-    while queue: # 큐가 빌 때까지
-        x, y = queue.popleft() # 현재 위치 꺼내기
-        for i in range(4): # 네 방향 탐색
-            nx, ny = x + dx[i],  y + dy[i] # 다음 위치
-            # 다음 위치가 범위 안에 있고, 아직 방문 안했다면
-            if 0 <= nx < n and 0 <= ny < m and not check[nx][ny]:
-                if cheeze[nx][ny]: # 만약 해당 위치가 치즈면
-                    cheeze[nx][ny] += 1 # 치즈 + 1 (치즈가 외부 공기와 만남, 큐에 저장 X)
-                else: # 해당 위치가 치즈가 아니면 (외부 공기)
-                    check[nx][ny] = True # 해당 위치 체크
-                    queue.append([nx, ny]) # 큐에 저장
+def checkAir(a, b):
+    queue = deque([])
+    queue.append([a, b])
+    visited = [[False] * m for _ in range(n)]
+    visited[a][b] = True
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny] == 0:
+                queue.append([nx, ny])
+                visited[nx][ny] = True
+                air[nx][ny] = True
 
-    # 치즈의 모든 부분을 돌면서
+answer = 0
+while True:
+    # 외부 공기 체크
+    air = [[False] * m for _ in range(n)]
+    checkAir(0, 0)
+
+    # 녹는 치즈 체크
+    cheese = []
     for i in range(n):
         for j in range(m):
-            if cheeze[i][j] >= 3: # 현재 위치의 치즈 값이 3이상이면 (기존 1 + 외부 공기 2 이상)
-                cheeze[i][j] = 0 # 현재 위치의 치즈 녹이기
-            elif 0 < cheeze[i][j] < 3: # 현재 위치의 치즈 값이 1이나 2면 (기존 1 or 기존 1 + 공기 1)
-                cheeze[i][j] = 1 # 원래 그대로인 값인 1로 갱신
+            if arr[i][j] == 1:
+                count = 0
+                for k in range(4):
+                    if air[i+dx[k]][j+dy[k]]:
+                        count += 1
+                if count >= 2:
+                    cheese.append([i, j])
 
-    answer += 1 # 1초 증가
+    if len(cheese) == 0:
+        break
+
+    for x, y in cheese:
+        arr[x][y] = 0
+
+    answer += 1
 
 print(answer)
