d78e813ebc1cfe1eaf4c77408466e9e72c558598
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-18 16:52:00 +0900

    [Silver II] Title: 나무 자르기, Time: 444 ms, Memory: 258492 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,21 +1,19 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-start = 0
-end = max(arr)
+start, end = 0, max(arr)
 answer = 0
 while start <= end:
-    count = 0
     mid = (start+end)//2
-    for i in arr:
-        if i > mid:
-            count += i-mid
-    if count < m:
-        end = mid-1
-    else:
-        start = mid+1
+    count = 0
+    for i in range(n):
+        if arr[i] > mid:
+            count += (arr[i] - mid)
+            
+    if count >= m:
         answer = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
 print(answer)
