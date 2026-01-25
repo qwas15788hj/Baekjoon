132f463de3154adee789e4d397fe57794daf028b
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-25 16:22:25 +0900

    [Gold IV] Title: LCS, Time: 436 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,12 +1,12 @@
 s1 = input()
 s2 = input()
+
 n = len(s1)
 m = len(s2)
-
-arr = [[0] * (m+1) for _ in range(n+1)]
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if s1[i-1] == s2[j-1]:
+arr = [[0] * (n+1) for _ in range(m+1)]
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        if s1[j-1] == s2[i-1]:
             arr[i][j] = arr[i-1][j-1] + 1
         else:
             arr[i][j] = max(arr[i-1][j], arr[i][j-1])
