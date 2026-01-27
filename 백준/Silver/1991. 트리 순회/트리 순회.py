e2a5d30fe91d10da8dd9fd3cf3183f0215ca44be
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-01-27 23:53:16 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,32 @@
+n = int(input())
+tree = dict()
+for _ in range(n):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+def pre_order(node):
+    if node == '.':
+        return
+    print(node, end='')
+    pre_order(tree[node][0])
+    pre_order(tree[node][1])
+
+def in_order(node):
+    if node == '.':
+        return
+    in_order(tree[node][0])
+    print(node, end='')
+    in_order(tree[node][1])
+
+def post_order(node):
+    if node == '.':
+        return
+    post_order(tree[node][0])
+    post_order(tree[node][1])
+    print(node, end='')
+
+pre_order('A')
+print()
+in_order('A')
+print()
+post_order('A')
