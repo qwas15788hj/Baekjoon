c1bfa5092c4c85d79c7b89a8de2496fcba764b9c
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-04-04 01:43:45 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 2904 ms, Memory: 61304 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,32 +1,35 @@
-import sys
-input = sys.stdin.readline
 from heapq import heappush, heappop
 
 n = int(input())
 m = int(input())
 arr = [[] for _ in range(n+1)]
-for i in range(m):
+for _ in range(m):
     a, b, c = map(int, input().split())
     arr[a].append([b, c])
-s, e = map(int, input().split())
+s, e = map(int ,input().split())
 
-queue = []
-heappush(queue, [0, s, [s]])
-visited = [1e9] * (n+1)
-visited[s] = 0
-
-check = []
-while queue:
-    d, x, v = heappop(queue)
-    if visited[x] < d:
+heap = []
+heappush(heap, [0, s])
+dist = [1e9] * (n+1)
+dist[s] = 0
+visited = [0] * (n+1)
+while heap:
+    c, x = heappop(heap)
+    if dist[x] < c:
         continue
-    if x == e:
-        check = v
-    for nx, nd in arr[x]:
-        if visited[nx] > d + nd:
-            visited[nx] = d + nd
-            heappush(queue, [d + nd, nx, v + [nx]])
+    for nx, nc in arr[x]:
+        if dist[nx] > c + nc:
+            dist[nx] = c + nc
+            visited[nx] = x
+            heappush(heap, [c + nc, nx])
 
-print(visited[e])
-print(len(check))
-print(*check)
+print(dist[e])
+a = [e]
+while True:
+    e = visited[e]
+    if e == 0:
+        break
+    a.append(e)
+
+print(len(a))
+print(*a[::-1])
