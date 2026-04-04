4d39433aa3f2580abdb12250822d9ec9b1bd0089
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-04-04 18:12:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 2448 ms, Memory: 623136 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -4,31 +4,33 @@ from itertools import permutations
 
 n = int(input())
 arr = list(map(int, input().split()))
-op = list(map(int, input().split()))
-op_arr = []
-for i in range(len(op)):
-    for j in range(op[i]):
-        op_arr.append(i)
+opr = list(map(int, input().split()))
 
-max_answer = -int(1e9)
-min_answer = int(1e9)
-for per in list(permutations(op_arr, len(op_arr))):
-    num = arr[0]
+op = []
+for i in range(4):
+    for _ in range(opr[i]):
+        op.append(i)
+
+perm = list(permutations(op, len(op)))
+answer1 = -int(1e9)
+answer2 = int(1e9)
+for per in perm:
+    result = arr[0]
     for i in range(n-1):
         if per[i] == 0:
-            num += arr[i+1]
+            result += arr[i+1]
         elif per[i] == 1:
-            num -= arr[i+1]
+            result -= arr[i+1]
         elif per[i] == 2:
-            num *= arr[i+1]
+            result *= arr[i+1]
         elif per[i] == 3:
-            if num < 0 and arr[i+1] > 0:
-                num = -1 * (num*(-1) // arr[i+1])
+            if result < 0 and arr[i+1] > 0:
+                result = -(-result // arr[i+1])
             else:
-                num //= arr[i+1]
+                result //= arr[i+1]
 
-    max_answer = max(max_answer, num)
-    min_answer = min(min_answer, num)
+    answer1 = max(answer1, result)
+    answer2 = min(answer2, result)
 
-print(max_answer)
-print(min_answer)
+print(answer1)
+print(answer2)
