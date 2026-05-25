a7a64eedd24f74981aa4acb3c9b09c58d42945d0
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-05-25 17:27:35 +0900

    Time: 4 ms (19.73%), Space: 43.9 MB (12.85%) - LeetHub

--- LeetCode/1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ LeetCode/1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,16 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String answer = "";
         int n = word1.length();
         int m = word2.length();
         int min = Math.min(n, m);
+        String answer = "";
 
         for(int i=0; i<min; i++) {
             answer += word1.charAt(i);
             answer += word2.charAt(i);
         }
-        answer += word1.substring(min, n);
-        answer += word2.substring(min, m);
+        
+        if(n > m) {
+            answer += word1.substring(m, n);
+        } else {
+            answer += word2.substring(n, m);
+        }
 
         return answer;
     }
