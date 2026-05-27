9a8830eb587e0bd33fd72fc9e16a212cd142ba55
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-05-27 23:43:04 +0900

    Time: 1 ms (95.68%), Space: 43.7 MB (87.83%) - LeetHub

---
+++ LeetCode/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.length;
+        List<Boolean> answer = new ArrayList<>();
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            max = Math.max(max, candies[i]);
+        }
+
+        for(int i=0; i<n; i++) {
+            if(candies[i] + extraCandies >= max) {
+                answer.add(true);
+            } else {
+                answer.add(false);
+            }
+        }
+
+        return answer;
+    }
+}
