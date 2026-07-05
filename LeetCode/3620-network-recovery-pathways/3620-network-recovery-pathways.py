63cee0c4dfb4204370690c83abb6d983e3131829
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-07-05 15:52:43 +0900

    Time: 998 ms (43.67%), Space: 60.1 MB (23.21%) - LeetHub

--- LeetCode/3620-network-recovery-pathways/3620-network-recovery-pathways.py
+++ LeetCode/3620-network-recovery-pathways/3620-network-recovery-pathways.py
@@ -2,6 +2,7 @@ from heapq import heappush, heappop
 
 class Solution:
     def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
+        # 최대 거리 측정
         n, m = len(online), 0
         for a, b, c in edges:
             m = max(m, c)
@@ -10,12 +11,12 @@ class Solution:
         arr = [[] for _ in range(n)]
         for a, b, c in edges:
             arr[a].append([b, c])
-
-        # 0 -> n-1까지 도달 가능한지 체크
+        
+        # 최대 길이 간선으로 도달할 수 있는지 체크(우선순위 큐 bfs)
         def bfs(m):
             heap = []
             heappush(heap, [0, 0])
-            visited = [float('INF')] * n
+            visited = [float('inf')] * n
             visited[0] = 0
             while heap:
                 d, x = heappop(heap)
@@ -25,16 +26,17 @@ class Solution:
                 if visited[x] < d:
                     continue
                 
-                # 다음 위치로 이동이 
-                # 1. k이하 / 2. 방문한 곳보다 가까운 거리 / 3. online / 4. 다음 이동 거리가 m이상일 때 => 이동 가능
+                # 다음 이동 위치가
+                # 방문길이보다 작고, k보다 작고, online이고, 최대 간선 기준인 m보다 클 때 => 이동 가능
                 for nx, nd in arr[x]:
-                    if d+nd <= k and visited[nx] > d+nd and online[nx] and nd >= m:
-                        heappush(heap, [d+nd, nx])
-                        visited[nx] = d+nd
+                    cost = d + nd
+                    if visited[nx] > cost and cost <= k and online[nx] and nd >= m:
+                        heappush(heap, [cost, nx])
+                        visited[nx] = cost
             
             return False
-        
-        # 이분탐색으로 최대 길이 찾기
+
+        # 간선의 최대 길이 찾기 (이분탐색)
         answer = -1
         start, end = 0, m
         while start <= end:
@@ -44,5 +46,5 @@ class Solution:
                 start = mid + 1
             else:
                 end = mid - 1
-    
+        
         return answer
