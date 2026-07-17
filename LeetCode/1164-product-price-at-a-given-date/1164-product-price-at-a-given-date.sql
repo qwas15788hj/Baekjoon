b4edd5cdbc735fe26d6ad48f2c20f37623f4a8f1
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-07-17 23:09:12 +0900

    Time: 511 ms (46.26%), Space: 0B (100%) - LeetHub

--- LeetCode/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ LeetCode/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -2,10 +2,10 @@
 SELECT a.product_id, IFNULL(b.new_price, 10) AS price
 FROM (SELECT DISTINCT product_id FROM Products) a
 LEFT JOIN (
-    SELECT product_id, new_price
+    SELECT product_id, new_price, change_date
     FROM Products
     WHERE (product_id, change_date) IN (
-        SELECT product_id, MAX(change_date)
+        SELECT product_id, MAX(change_date) AS max_date
         FROM Products
         WHERE change_date <= '2019-08-16'
         GROUP BY product_id
