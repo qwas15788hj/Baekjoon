9c6305d41d3f3bf42fed233c45904f0e150329aa
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-07-23 22:45:57 +0900

    [level 3] Title: 블록 이동하기, Time: 16.66 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -3,12 +3,11 @@ from collections import deque
 def solution(board):
     answer = 0
     n = len(board)
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
     queue = deque([[0, 0, 0, 1, 0]])
     visited = set()
-    visited.add((0, 0, 0, 1))
     visited.add((0, 0, 0, 1))
     while queue:
         x1, y1, x2, y2, c = queue.popleft()
@@ -19,71 +18,76 @@ def solution(board):
         for k in range(4):
             nx1, ny1 = x1 + dx[k], y1 + dy[k]
             nx2, ny2 = x2 + dx[k], y2 + dy[k]
-            # 범위 안에 있고,
             if 0 <= nx1 < n and 0 <= ny1 < n and 0 <= nx2 < n and 0 <= ny2 < n and board[nx1][ny1] == 0 and board[nx2][ny2] == 0:
                 if (nx1, ny1, nx2, ny2) not in visited and (nx2, ny2, nx1, ny1) not in visited:
-                    visited.add((nx1, ny1, nx2, ny2))
                     queue.append([nx1, ny1, nx2, ny2, c+1])
+                    visited.add((nx1, ny1, nx2, ny2))
+        
+        # 왼쪽로봇(1), 오른쪽로봇(2) / 2가 1보다 오른쪽아래에 위치 -> 목적지와 가까워야함
+        # 가로일 때
+        if x1 == x2 and abs(y1-y2) == 1:
+            # 왼쪽 로봇이 위로 이동
+            nx1, ny1 = x1-1, y1+1
+            cx1, cy1 = x1-1, y1
+            if 0 <= nx1 < n and 0 <= ny1 < n and 0 <= cx1 < n and 0 <= cy1 < n and board[nx1][ny1] == 0 and board[cx1][cy1] == 0:
+                if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
+                    queue.append([nx1, ny1, x2, y2, c+1])
+                    visited.add((nx1, ny1, x2, y2))
+            # 왼쪽 로봇이 아래로 이동
+            nx1, ny1 = x1+1, y1+1
+            cx1, cy1 = x1+1, y1
+            if 0 <= nx1 < n and 0 <= ny1 < n and 0 <= cx1 < n and 0 <= cy1 < n and board[nx1][ny1] == 0 and board[cx1][cy1] == 0:
+                if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
+                    # 1, 2 위치 변경
+                    queue.append([x2, y2, nx1, ny1, c+1])
+                    visited.add((x2, y2, nx1, ny1))
+            # 오른쪽 로봇이 위로 이동
+            nx2, ny2 = x2-1, y2-1
+            cx2, cy2 = x2-1, y2
+            if 0 <= nx2 < n and 0 <= ny2 < n and 0 <= cx2 < n and 0 <= cy2 < n and board[nx2][ny2] == 0 and board[cx2][cy2] == 0:
+                if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
+                    # 1, 2 위치 변경
+                    queue.append([nx2, ny2, x1, y1, c+1])
+                    visited.add((nx2, ny2, x1, y1))
+            # 오른쪽 로봇이 아래로 이동
+            nx2, ny2 = x2+1, y2-1
+            cx2, cy2 = x2+1, y2
+            if 0 <= nx2 < n and 0 <= ny2 < n and 0 <= cx2 < n and 0 <= cy2 < n and board[nx2][ny2] == 0 and board[cx2][cy2] == 0:
+                if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
+                    queue.append([x1, y1, nx2, ny2, c+1])
+                    visited.add((x1, y1, nx2, ny2))            
             
-        # 방향 변경 (변경시 반드시 2가 1보다 우측이나 아래에 있어야 함)
-        # 로봇이 가로 상태면
-        if (x1 == x2) and (abs(y1-y2) == 1):
-            # 왼쪽에 있는 1이 위로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            nx1, ny1 = x2-1, y2
-            if 0 <= nx1 < n and 0 <= ny1 < n and board[nx1][ny1] == 0 and 0 <= x2-1 < n and 0 <= y2-1 < n and board[x2-1][y2-1] == 0:
+        # 세로일 때    
+        else:
+            # 위쪽 로봇이 왼쪽으로 이동
+            nx1, ny1 = x1+1, y1-1
+            cx1, cy1 = x1, y1-1
+            if 0 <= nx1 < n and 0 <= ny1 < n and 0 <= cx1 < n and 0 <= cy1 < n and board[nx1][ny1] == 0 and board[cx1][cy1] == 0:
                 if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
-                    visited.add((nx1, ny1, x2, y2))
                     queue.append([nx1, ny1, x2, y2, c+1])
-            # 왼쪽에 있는 1이 아래로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            # 아래로 이동이기에 저장은 반대로
-            nx1, ny1 = x2+1, y2
-            if 0 <= nx1 < n and 0 <= ny1 < n and board[nx1][ny1] == 0 and 0 <= x2+1 < n and 0 <= y2-1 < n and board[x2+1][y2-1] == 0:
-                if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
-                    visited.add((x2, y2, nx1, ny1))
-                    queue.append([x2, y2, nx1, ny1, c+1])
-            # 오른쪽에 있는 2가 위로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            # 2가 위로 이동이기에 저장은 반대로
-            nx2, ny2 = x1-1, y1
-            if 0 <= nx2 < n and 0 <= ny2 < n and board[nx2][ny2] == 0 and 0 <= x1-1 < n and 0 <= y1+1 < n and board[x1-1][y1+1] == 0:
-                if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
-                    visited.add((nx2, ny2, x1, y1))
-                    queue.append([nx2, ny2, x1, y1, c+1])
-            # 오른쪽에 있는 2가 아래로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            nx2, ny2 = x1+1, y1
-            if 0 <= nx2 < n and 0 <= ny2 < n and board[nx2][ny2] == 0 and 0 <= x1+1 < n and 0 <= y1+1 < n and board[x1+1][y1+1] == 0:
-                if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
-                    visited.add((x1, y1, nx2, ny2))
-                    queue.append([x1, y1, nx2, ny2, c+1])    
-                
-                
-        # 방향 변경 (변경시 반드시 2가 1보다 우측이나 아래에 있어야 함)
-        # 로봇이 세로 상태면
-        if (abs(x1-x2) == 1) and (y1 == y2):
-            # 위쪽에 있는 1이 왼쪽로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            nx1, ny1 = x2, y2-1
-            if 0 <= nx1 < n and 0 <= ny1 < n and board[nx1][ny1] == 0 and 0 <= x2-1 < n and 0 <= y2-1 < n and board[x2-1][y2-1] == 0:
-                if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
                     visited.add((nx1, ny1, x2, y2))
-                    queue.append([nx1, ny1, x2, y2, c+1])
-            # 위쪽에 있는 1이 오른쪽로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            # 1이 오른쪽으로 이동이기에 반대로 저장
-            nx1, ny1 = x2, y2+1
-            if 0 <= nx1 < n and 0 <= ny1 < n and board[nx1][ny1] == 0 and 0 <= x2-1 < n and 0 <= y2+1 < n and board[x2-1][y2+1] == 0:
+            # 위쪽 로봇이 오른쪽으로 이동
+            nx1, ny1 = x1+1, y1+1
+            cx1, cy1 = x1, y1+1
+            if 0 <= nx1 < n and 0 <= ny1 < n and 0 <= cx1 < n and 0 <= cy1 < n and board[nx1][ny1] == 0 and board[cx1][cy1] == 0:
                 if (nx1, ny1, x2, y2) not in visited and (x2, y2, nx1, ny1) not in visited:
-                    visited.add((x2, y2, nx1, ny1))
+                    # 1, 2 위치 변경
                     queue.append([x2, y2, nx1, ny1, c+1])
-            # 아래쪽에 있는 2가 왼쪽로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            # 2가 왼쪽으로 이동하기에 반대로 저장
-            nx2, ny2 = x1, y1-1
-            if 0 <= nx2 < n and 0 <= ny2 < n and board[nx2][ny2] == 0 and 0 <= x1+1 < n and 0 <= y1-1 < n and board[x1+1][y1-1] == 0:
+                    visited.add((x2, y2, nx1, ny1))
+            # 아래쪽 로봇이 왼쪽으로 이동
+            nx2, ny2 = x2-1, y2-1
+            cx2, cy2 = x2, y2-1
+            if 0 <= nx2 < n and 0 <= ny2 < n and 0 <= cx2 < n and 0 <= cy2 < n and board[nx2][ny2] == 0 and board[cx2][cy2] == 0:
                 if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
-                    visited.add((nx2, ny2, x1, y1))
+                    # 1, 2 위치 변경
                     queue.append([nx2, ny2, x1, y1, c+1])
-            # 아래쪽에 있는 2가 오른쪽으로 이동, 이동 위치와 이동 시 걸리는 곳 모두 범위 안에 있으며 0이여야함
-            nx2, ny2 = x1, y1+1
-            if 0 <= nx2 < n and 0 <= ny2 < n and board[nx2][ny2] == 0 and 0 <= x1+1 < n and 0 <= y1+1 < n and board[x1+1][y1+1] == 0:
+                    visited.add((nx2, ny2, x1, y1))
+            # 아래쪽 로봇이 오른쪽으로 이동
+            nx2, ny2 = x2-1, y2+1
+            cx2, cy2 = x2, y2+1
+            if 0 <= nx2 < n and 0 <= ny2 < n and 0 <= cx2 < n and 0 <= cy2 < n and board[nx2][ny2] == 0 and board[cx2][cy2] == 0:
                 if (x1, y1, nx2, ny2) not in visited and (nx2, ny2, x1, y1) not in visited:
+                    queue.append([x1, y1, nx2, ny2, c+1])
                     visited.add((x1, y1, nx2, ny2))
-                    queue.append([x1, y1, nx2, ny2, c+1])    
     
     return answer
