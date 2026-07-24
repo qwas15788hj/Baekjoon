96bc9ffcb6d969fbd56b589f15307d7417fc222d
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-07-25 00:38:06 +0900

    Time: 59 ms (95.51%), Space: 20.2 MB (94.65%) - LeetHub

--- LeetCode/0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
+++ LeetCode/0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
@@ -5,18 +5,18 @@ class Solution:
         n = len(graph)
 
         queue = deque([])
-        visited = [[0] * (1<<n) for _ in range(n)]
+        visited = [[-1] * (1<<n) for _ in range(n)]
         for i in range(n):
-            queue.append([i, 1<<i])
-            visited[i][1<<i] = 1
+            queue.append([i, (1<<i)])
+            visited[i][1<<i] = 0
         
         while queue:
             x, mask = queue.popleft()
             if mask == (1<<n)-1:
-                return visited[x][mask] - 1
+                return visited[x][mask]
             
             for nx in graph[x]:
                 nmask = mask | 1<<nx
-                if not visited[nx][nmask]:
+                if visited[nx][nmask] == -1:
                     visited[nx][nmask] = visited[x][mask] + 1
                     queue.append([nx, nmask])
