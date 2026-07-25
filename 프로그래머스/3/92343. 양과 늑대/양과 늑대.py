c2c6ed287c742c98a989eaa85171755f2b31a9f2
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-07-25 16:40:58 +0900

    [level 3] Title: 양과 늑대, Time: 24.11 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,8 +2,8 @@ from collections import deque
 
 def solution(info, edges):
     answer = 0
-    n, m = len(info), len(edges)
     
+    n, m = len(info), len(edges)
     arr = [[] for _ in range(n)]
     parent = dict()
     for a, b in edges:
@@ -17,13 +17,12 @@ def solution(info, edges):
         x, mask, s, w = queue.popleft()
         answer = max(answer, s)
         
-        # 0 ~ n-1까지 방문 가능한 곳 찾기
         for nx in range(n):
-            # 조건1 : 아직 방문 안한 곳
-            if not (mask & (1<<nx)):
-                # 조건2 : 부모가 방문한 곳이여야 함 (0제외)
-                if nx == 0 or (mask & (1<<parent[nx])):
-                    nmask = mask | (1<<nx)
+            # 아직 방문X
+            if not (mask & 1<<nx):
+                # 부모는 방문했어야 함 (0 제외)
+                if nx == 0 or (mask & 1<<parent[nx]):
+                    nmask = mask | 1<<nx
                     if info[nx] == 0:
                         queue.append([nx, nmask, s+1, w])
                         visited[nx][nmask] = True
