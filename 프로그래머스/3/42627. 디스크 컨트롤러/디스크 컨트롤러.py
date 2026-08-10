ff7b9fcb96b38187a897bdf4a61b31157b1b3486
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2026-08-10 23:43:47 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.43 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,31 +1,29 @@
 from heapq import heappush, heappop
 
 def solution(jobs):
-    n = len(jobs)
-    answer = [0] * n
+    answer = 0
     
-    for i in range(n):
-        jobs[i].append(i)
+    n = len(jobs)
     arr = []
-    time = 0
-    idx = 0
+    for i in range(n):
+        jobs[i] = jobs[i] + [i]
     jobs.sort(key=lambda x:x[0])
-    while True:
-        if 0 not in answer:
-            break
-
-        while idx != n:
-            if jobs[idx][0] <= time:
-                heappush(arr, [jobs[idx][1], jobs[idx][0], jobs[idx][2]])
-                idx += 1
-            else:
-                break
+    
+    cnt = 0
+    idx = 0
+    time = jobs[0][0]
+    while cnt != n:
+        while idx < n and jobs[idx][0] <= time:
+            heappush(arr, [jobs[idx][1], jobs[idx][0], jobs[idx][2]])
+            idx += 1
         
-        if len(arr):
-            job = heappop(arr)
-            time += job[0]
-            answer[job[2]] = time-job[1]
-        elif len(arr) == 0 and idx <= n:
+        if len(arr) == 0 and idx < n:
             time = jobs[idx][0]
-            
-    return sum(answer)//n
+            continue
+        else:
+            t, s, i = heappop(arr)
+            answer += (time + t) - s
+            time += t
+            cnt += 1
+    
+    return answer//n
